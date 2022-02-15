7ad1a6c19ac30dce24e5ff2e988b631312c2f4fb
irb1s <kolyirb1s@gmail.com>
2022-02-15 21:44:15 +0500

    Commit

---
+++ components/LessonCard/LessonCard.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import {TimeTable} from "../../models/TimeTable";
+
+const Card: FC<TimeTable> = (props) => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Card;
